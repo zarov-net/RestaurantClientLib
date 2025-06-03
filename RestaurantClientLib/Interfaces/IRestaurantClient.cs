c40700ecebda04fe000412f9405efc77f19c248b
Евгений Елизаров <jony.saint@bk.ru>
2025-06-04 02:06:49 +0300

    remove other files

--- RestaurantClientLib/Interfaces/IRestaurantClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using RestaurantClientLib.Models;
-
-namespace RestaurantClientLib.Interfaces;
-
-public interface IRestaurantClient
-{
-    Task<Dish[]> GetDishesAsync(CancellationToken cancellationToken = default);
-    Task<bool> SendOrderAsync(Order order, CancellationToken cancellationToken = default);
-}
